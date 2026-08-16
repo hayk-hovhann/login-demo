bc7031baa7178941f544db61ee2600d607b2e72c
hayk-hovhann <hayk.hovhannisyan27.88@gmail.com>
2026-08-16 12:36:14 +0400

    fix(backend): type the request in AuthenticatedGuard

--- backend/src/auth/guards/authenticated.guard.ts
+++ backend/src/auth/guards/authenticated.guard.ts
@@ -1,8 +1,9 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import type { Request } from 'express';
 
 @Injectable()
 export class AuthenticatedGuard implements CanActivate {
   canActivate(context: ExecutionContext): boolean {
-    return context.switchToHttp().getRequest().isAuthenticated();
+    return context.switchToHttp().getRequest<Request>().isAuthenticated();
   }
 }
